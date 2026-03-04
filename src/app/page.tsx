6ffeab7046218237ae174092a4d2ac416ff2f924
Yagyesh <saraswatthrust@gmail.com>
2026-03-04 14:37:42 +0530

    Prepare landing page for authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return (
-    <Button onClick={() => toast.success("Hail hitler !!")}>
-      Click me
-    </Button>
+
+  return(
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-background">
+      <h1 className="text-2xl font-semibold">Welcome to Resonant</h1>
+      <div className="flex items-center gap-4">
+        <OrganizationSwitcher />
+        <UserButton />
+      </div>
+    </div>
   );
 };
