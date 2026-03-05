3d37d0333c9e85e871e9fc1ac880728e48260f97
Yagyesh <saraswatthrust@gmail.com>
2026-03-05 19:51:35 +0530

    feat: add VoicePreviewPlaceholder component to text-to-speech view

--- src/features/text-to-speech/views/text-to-speech-view.tsx
+++ src/features/text-to-speech/views/text-to-speech-view.tsx
@@ -1,5 +1,5 @@
 import { TextInputPanel } from "@/features/text-to-speech/components/text-input-panel";
-// import { VoicePreviewPlaceholder } from "../components/voice-preview-placeholder";
+import { VoicePreviewPlaceholder } from "@/features/text-to-speech/components/voice-preview-placeholder";
 // import { SettingsPanel } from "../components/settings-panel";
 
 export function TextToSpeechView() {
@@ -8,7 +8,7 @@ export function TextToSpeechView() {
         <div className="flex min-h-0 flex-1 overflow-hidden">
             <div className="flex min-h-0 flex-1 flex-col">
                 <TextInputPanel />
-                {/* <VoicePreviewPlaceholder /> */}
+                <VoicePreviewPlaceholder />
             </div>
             {/* <SettingsPanel /> */}
         </div>
