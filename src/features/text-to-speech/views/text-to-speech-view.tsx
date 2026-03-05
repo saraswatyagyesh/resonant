3adfc9934093ac78b5747846c0adb5b01d35e65f
Yagyesh <saraswatthrust@gmail.com>
2026-03-05 17:43:02 +0530

    feat: add TextToSpeech layout and view components for text-to-speech functionality

---
+++ src/features/text-to-speech/views/text-to-speech-view.tsx
@@ -0,0 +1,16 @@
+// import { TextInputPanel } from "../components/text-input-panel";
+// import { VoicePreviewPlaceholder } from "../components/voice-preview-placeholder";
+// import { SettingsPanel } from "../components/settings-panel";
+
+export function TextToSpeechView() {
+
+    return(
+        <div>
+            <div>
+                {/* <TextInputPanel />
+                <VoicePreviewPlaceholder /> */}
+            </div>
+            {/* <SettingsPanel /> */}
+        </div>
+    );
+};
