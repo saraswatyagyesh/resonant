39c99fc6eff5bb532c756a1a42678bc2b3adbf12
Yagyesh <saraswatthrust@gmail.com>
2026-03-08 00:43:53 +0530

    feat: enhance TextToSpeech component with voice selection and context management

--- src/app/(dashboard)/text-to-speech/page.tsx
+++ src/app/(dashboard)/text-to-speech/page.tsx
@@ -1,10 +1,17 @@
-import { TextToSpeechView } from '@/features/text-to-speech/views/text-to-speech-view';
 import type { Metadata } from 'next';
+import { TextToSpeechView } from '@/features/text-to-speech/views/text-to-speech-view';
+import { trpc, HydrateClient, prefetch } from "@/trpc/server";
 
 export const metadata: Metadata = { title: "Text to speech" };
 
-export default function TextToSpeech() {
+export default async function TextToSpeech({searchParams,}:{searchParams: Promise<{text?: string; voiceId?: string;}>;}) {
+    const { text, voiceId } = await searchParams;
+
+    prefetch(trpc.voices.getAll.queryOptions());
+    
     return(
-        <TextToSpeechView />
+        <HydrateClient>
+            <TextToSpeechView initialValues={{ text, voiceId }}/>
+        </HydrateClient>
     );
 };
