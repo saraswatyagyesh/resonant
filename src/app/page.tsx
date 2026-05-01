eab9ccd635882ce81a74e79597c270022de0f334
Yagyesh <saraswatthrust@gmail.com>
2026-05-01 14:15:31 +0530

    chore: remove all files from the project repository

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ProjectsView } from "@/features/projects/components/projects-view";
-
-const Home = () => {
-  return <ProjectsView />
-};
-
-export default Home;
--- src/inngest/client.ts
+++
@@ -1,8 +0,0 @@
-import { Inngest } from "inngest";
-import { sentryMiddleware } from "@inngest/middleware-sentry";
-
-// Create a client to send and receive events
-export const inngest = new Inngest({ 
-  id: "polaris",
-  middleware: [sentryMiddleware()],
-});
